4a798ad58447b925f919deb94b2a2597887e5b5a
John Riselvato <jdriselvato@gmail.com>
2016-08-27 11:33:28 -0400

    Initial Commit

---
+++ Blue Bean DropperTests/Blue_Bean_DropperTests.swift
@@ -0,0 +1,36 @@
+//
+//  Blue_Bean_DropperTests.swift
+//  Blue Bean DropperTests
+//
+//  Created by John Riselvato on 8/27/16.
+//  Copyright © 2016 John Riselvato. All rights reserved.
+//
+
+import XCTest
+@testable import Blue_Bean_Dropper
+
+class Blue_Bean_DropperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
